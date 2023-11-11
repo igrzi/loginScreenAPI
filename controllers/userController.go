161ac2c7922bf4b3ec2c5cb424bd5dbab2246778
igrzi <igorzimmer01@gmail.com>
2023-11-11 01:45:18 -0300

    feat: Checking if the user is already on database

--- controllers/userController.go
+++ controllers/userController.go
@@ -3,13 +3,16 @@ package controllers
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"errors"
 
 	"github.com/gin-gonic/gin"
 	"github.com/igrzi/loginScreenAPI/initializers"
 	"github.com/igrzi/loginScreenAPI/models"
+	"gorm.io/gorm"
 )
 
 func UserCreate(c *gin.Context) {
+
 	// Get data of user from request body
 	var userData struct {
 		Email    string
@@ -17,25 +20,38 @@ func UserCreate(c *gin.Context) {
 	}
 	c.Bind(&userData)
 
-	// Encrypt password
+	if !checkIfUserExists(userData.Email) {
+		// Encrypt password
 
-	encryptedPassword := sha256.New()
-	encryptedPassword.Write([]byte(userData.Password))
-	hashedPassword := hex.EncodeToString(encryptedPassword.Sum(nil))
+		encryptedPassword := sha256.New()
+		encryptedPassword.Write([]byte(userData.Password))
+		hashedPassword := hex.EncodeToString(encryptedPassword.Sum(nil))
 
-	// Create a user on the database
-	user := models.User{Email: userData.Email, Password: hashedPassword}
+		// Create a user on the database
+		user := models.User{Email: userData.Email, Password: hashedPassword}
+		result := initializers.DB.Create(&user) // pass pointer of data to Create
 
-	result := initializers.DB.Create(&user) // pass pointer of data to Create
+		if result.Error != nil {
+			c.Status(400)
+			return
+		}
 
-	if result.Error != nil {
-		c.Status(400)
-		return
+		// Return a 200 status
+
+		c.Status(200)
+	} else {
+		c.JSON(409, "User alredy on database")
 	}
+}
 
-	// Return a 200 status
+func checkIfUserExists(email string) bool {
+	var user models.User
 
-	c.Status(200)
+	// Check if the user with the provided email exists
+	result := initializers.DB.Where("email = ?", email).First(&user)
+	userExists := !errors.Is(result.Error, gorm.ErrRecordNotFound)
+
+	return userExists
 }
 
 func UserCheck(c *gin.Context) {
