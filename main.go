513bf2b471827cf8e0cbf98fae9f6e498cc1772a
igrzi <igorzimmer01@gmail.com>
2023-11-11 00:45:40 -0300

    feat: Implemented correct handler function

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/igrzi/loginScreenAPI/controllers"
 	"github.com/igrzi/loginScreenAPI/initializers"
 )
 
@@ -13,11 +14,7 @@ func init() {
 func main() {
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/submitUser", controllers.UserCreate)
 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
