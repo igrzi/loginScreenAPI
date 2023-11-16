a17426866c53c5c014604796f3063e9bc4933103
igrzi <igorzimmer01@gmail.com>
2023-11-15 23:23:30 -0300

    feat: implemented CORS handling

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/igrzi/loginScreenAPI/controllers"
 	"github.com/igrzi/loginScreenAPI/initializers"
@@ -13,6 +14,10 @@ func init() {
 
 func main() {
 	r := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://127.0.0.1:5500"}
+	r.Use(cors.New(config))
 
 	r.POST("/submitUser", controllers.UserCreate)
 	r.GET("/checkUser", controllers.UserCheck)
