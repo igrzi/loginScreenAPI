556d41d0551dc7010ccae7dffd6a1c8f8fe91552
igrzi <igorzimmer01@gmail.com>
2023-11-15 23:26:11 -0300

    feat: Handle user auth form, submit to API

--- static/script.js
+++ static/script.js
@@ -15,10 +15,42 @@ document.addEventListener("DOMContentLoaded", function () {
         const emailValue = emailInput.value;
         const passwordValue = passwordInput.value;
 
-        // Print the values to the console
-        if (emailValue != "" && passwordValue != ""){
-            console.log("Email:", emailValue);
-            console.log("Password:", passwordValue);
+        // Check if both email and password are provided
+        if (emailValue !== "" && passwordValue !== "") {
+            // Construct the API URLc
+            const apiUrl = "http://localhost:3000/submitUser";
+
+            // Create an object with the email and password
+            const data = {
+                Email: emailValue,
+                Password: passwordValue
+            };
+
+            // Make a POST request to the API
+            fetch(apiUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => {
+                // Check if the response status is 200
+                if (response.status === 200) {
+                    // Redirect to another HTML page
+                    window.location.href = "/static/pages/login.html";
+                } else {
+                    // Handle other response statuses here
+                    return response.json();
+                }
+            })
+            .then(result => {
+                // Handle the API response here
+                console.log("API Response:", result);
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
         }
     });
 });
